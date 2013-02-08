054879a8034e48a91d57bc33a3797f2ba044bc52
Weicheng Huang <whuang23@hawk.iit.edu>
2013-02-08 16:33:41 -0600

    activity2 and nim test

--- cs331/src/activity2.rkt
+++ cs331/src/activity2.rkt
@@ -4,11 +4,19 @@
         [else (* x (power x (- y 1)))]
         ))
 
-(define (range x xx)
+(define (range1 x xx)
   (cond [(= x (* 2 xx)) null]
-        [else (cons (- x xx) (range (+ x 1) xx))]
+        [else (cons (- x xx) (range1 (+ x 1) xx))]
         ))
 
+(define (range2 x)
+  (range-aux 0 x))
+  
+(define (range-aux i x)
+  (if (< i x) 
+      (cons i (range-aux (+ i 1) x))
+            '()))
+   
 (define (myappend xx yy)
   (cond [(null? xx) (cond [(null? yy) null]
                           [else (myappend yy null)])]
@@ -18,6 +26,14 @@
 (define aa '(1 23 4 5))
 (define bb '(2 3 4 5))
 
-(define (reverse xx)
-  (conds [(null? xx) null]
-         [
+(define (bad-reverse xx)
+  (if (null? xx) '()
+      (append (bad-reverse (car xx)) (list (car xx))))
+  )
+
+(define (my-reverse-aux xx acc)
+  (if (null? xx) acc
+      (my-reverse-aux (cdr xx) (cons (car xx) acc))))
+
+(define (my-reverse xx)
+  (my-reverse-aux xx '()))
