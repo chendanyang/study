e921e421a9854fb14fd601c1850a7084ba3cd0b1
Weicheng Huang <whuang23@hawk.iit.edu>
2013-05-03 11:55:46 -0500

    java final lab

---
+++ EclipseWorkShop/final/src/Server.java
@@ -0,0 +1,30 @@
+
+class Server {
+	ObjArray data;
+	
+	public Server() {
+		data = new ObjArray();
+	}
+	public void addData(Athlete a){
+		data.add(a);
+	}
+	public void delData(int index){
+		
+	}
+	public void list(){
+		
+	}
+	public void displayAthlete(int index){
+		
+	}
+	public void displayCountry(String c){
+		
+	}
+	public void displayRevenue(int index){
+		
+	}
+	public void displayVenue(int index){
+		
+	}
+	
+}
